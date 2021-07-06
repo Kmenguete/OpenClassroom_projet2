5d358f379195cc0bf11ca05fb67fae5bb3169760
unknown <menguetekevin@gmail.com>
2021-07-06 11:41:57 +0200

    Les données des pages suivantes

--- get_next_page.py
+++ get_next_page.py
@@ -1,24 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-page = requests.get("http://books.toscrape.com/")  # I am downloading a web page using requests
-soup = BeautifulSoup(page.content, 'html.parser')  # I am creating an instance of the BeautifulSoup class to parse the document
-for web in soup.find('ul', class_='nav nav-list').find('li').find('ul').find_all('li'):
-    href = web.a.get('href')
-    url = "http://books.toscrape.com/{}".format(href.replace("index", "page-1"))
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    next_li = soup.find('li', class_='next')
-    incrementor = 1
-    while next_li is not None:
-        url = "http://books.toscrape.com/{}".format(href.replace("index", "page-{}".format(incrementor)))
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        next_li = soup.find('li', class_='next')
-        print(url)
-        incrementor += 1
+from get_books_for_category import get_books_for_category
 
 
 def get_next_page():
-    return url
+    page = requests.get("http://books.toscrape.com/")  # I am downloading a web page using requests
+    soup = BeautifulSoup(page.content,
+                         'html.parser')  # I am creating an instance of the BeautifulSoup class to parse the document
+    for web in soup.find('ul', class_='nav nav-list').find('li').find('ul').find_all('li'):
+        href = web.a.get('href')
+        url = "http://books.toscrape.com/{}".format(href.replace("index", "page-1"))
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        next_li = soup.find('li', class_='next')
+        incrementor = 1
+        while next_li is not None:
+            url = "http://books.toscrape.com/{}".format(href.replace("index", "page-{}".format(incrementor)))
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            next_li = soup.find('li', class_='next')
+            print(url)
+            get_books_for_category(url)
+            incrementor += 1
+
+
+get_next_page()
+# return url
