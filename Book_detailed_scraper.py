e14f4cc2b6084be430a3c3b4478da4abae03f3e7
unknown <menguetekevin@gmail.com>
2021-07-23 11:30:42 +0200

    projet 2 version finale

--- Book_detailed_scraper.py
+++ Book_detailed_scraper.py
@@ -1,27 +1,17 @@
+import os
 import time
-import csv
-
 import requests
 from bs4 import BeautifulSoup
 
 
-def store_book_information(product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax,
-                           number_available, product_description_head, product_description, category,
-                           review_rating,  image_url):
+def store_book_information(category_name, book_information_as_list):
+    converted_list = [str(element) for element in book_information_as_list]
 
-    with open('book_information_storage.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["product_page_url", product_page_url])
-        writer.writerow(["universal_product_code(upc)", universal_product_code])
-        writer.writerow(["title", title])
-        writer.writerow(["price_including_tax", price_including_tax])
-        writer.writerow(["price_excluding_tax ", price_excluding_tax])
-        writer.writerow(["number_available", number_available])
-        writer.writerow(["product_description_head", product_description_head])
-        writer.writerow(["product_description", product_description])
-        writer.writerow(["category", category])
-        writer.writerow(["review_rating ", review_rating])
-        writer.writerow(["image_url", image_url])
+    book_information_as_string = "|".join(converted_list).replace("\n", "")
+    print(book_information_as_string)
+    with open('books/{}.csv'.format(category_name), 'a', encoding='utf-8') as file:
+        file.write("\n")
+        file.write(f'{book_information_as_string}\n')
 
 
 def book_extract_information(url):
@@ -42,9 +32,22 @@ def book_extract_information(url):
     category = soup.find_all('a')[3].get_text()
     review_rating = soup.find_all('tr')[6].get_text()
     image_url = soup.find_all('img')[0]
-    store_book_information(product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax,
-                           number_available, product_description_head, product_description, category,
-                           review_rating, image_url)
+    image_link1 = str(image_url).replace('<img alt="{}" src="../..'.format(title.replace('&', '&amp;')), '')
+    image_link2 = str(image_link1).replace('"/>', '')
+    image_url = "https://books.toscrape.com" + image_link2
+    print(image_url)
+    directory = 'books/{}'.format(category)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    response = requests.get(image_url)
+    file = open("{}/{}.jpg".format(directory, title), "wb")
+    file.write(response.content)
+    file.close()
+    book_information_as_list = [product_page_url, universal_product_code, title, price_including_tax,
+                                price_excluding_tax, number_available, product_description_head, product_description,
+                                category, review_rating, image_url]
+    return book_information_as_list
 
 
 url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
